9cdb86b629e1fd4c64548e825c23a897e1df6e2d
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-12-12 13:36:25 -0200

    first commit

---
+++ src/main/java/com/gustavopm1/requestssniffer/RequestsSnifferApplication.java
@@ -0,0 +1,14 @@
+package com.gustavopm1.requestssniffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestsSnifferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestsSnifferApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/gustavopm1/requestssniffer/RequestsSnifferApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gustavopm1.requestssniffer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RequestsSnifferApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
